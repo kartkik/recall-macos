0e957e81155a1e21086ea8e2f268b88712d0ca3e
karthik.krishna <karthikkrishna104@gmail.com>
2026-05-07 21:38:59 +0530

    Initial Commit

---
+++ Recall/RecallApp.swift
@@ -0,0 +1,32 @@
+//
+//  RecallApp.swift
+//  Recall
+//
+//  Created by twixx  on 07/05/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct RecallApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
